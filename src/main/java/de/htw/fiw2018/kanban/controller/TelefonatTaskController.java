6b387941ca480b3ae2d6039a54fc7193502b2d6c
Michael Dähnert <michael.daehnert@soprasteria.com>
2018-12-14 15:44:47 +0100

    Telefonat callable.

--- src/main/java/de/htw/fiw2018/kanban/controller/TelefonatTaskController.java
+++ src/main/java/de/htw/fiw2018/kanban/controller/TelefonatTaskController.java
@@ -1,4 +1,15 @@
 package de.htw.fiw2018.kanban.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/telefonat")
 public class TelefonatTaskController {
+
+    @RequestMapping(path = "/hello")
+    public String hello(){
+        return "Hello Frau S.!";
+    }
+
 }
