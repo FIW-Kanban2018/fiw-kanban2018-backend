a49e1f013472e7058b9a755da7ceac11d5d39bde
Kübra Demir <s0553666@htw-berlin.de>
2019-02-07 21:19:58 +0100

    ategoryController überarbeitet

--- src/main/java/de/htw/fiw2018/kanban/controller/CategoryController.java
+++ src/main/java/de/htw/fiw2018/kanban/controller/CategoryController.java
@@ -36,53 +36,51 @@ public class CategoryController extends GenericTaskController {
 
     @GetMapping(path = "/referatsueber")
     public List<GenericTaskEntity> findAllReferatsueber() {
-        this.referatList.addAll(dringendTaskRepository.findAllByCardCategoryReferat());
-        this.referatList.addAll(telefonatTaskRepository.findAllByCardCategoryReferat());
-        this.referatList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryReferat());
-        this.referatList.addAll(sonstigesTaskRepository.findAllByCardCategoryReferat());
-        this.referatList.addAll(veranstaltungTaskRepository.findAllByCardCategoryReferat());
-        this.referatList.addAll(wanderkarteTaskRepository.findAllByCardCategoryReferat());
-//        referatList.forEach((temp) -> {
-//            System.out.println(temp.toString());
-//        });
-//        for (Map.Entry<String, String> entry : referatList.entrySet())
-//        {
-//            System.out.println(entry.getKey() + "/" + entry.getValue());
-//        }
-        return this.referatList;
+        List<GenericTaskEntity> referatList = new ArrayList<>();
+        referatList.addAll(dringendTaskRepository.findAllByCardCategoryReferat());
+        referatList.addAll(telefonatTaskRepository.findAllByCardCategoryReferat());
+        referatList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryReferat());
+        referatList.addAll(sonstigesTaskRepository.findAllByCardCategoryReferat());
+        referatList.addAll(veranstaltungTaskRepository.findAllByCardCategoryReferat());
+        referatList.addAll(wanderkarteTaskRepository.findAllByCardCategoryReferat());
+
+        return referatList;
     }
 
     @GetMapping(path = "/geschaeft")
-    public Iterable<GenericTaskEntity> findAllGeschäftszimmer() {
-        this.geschaeftList.addAll(dringendTaskRepository.findAllByCardCategoryGeschaeft());
-        this.geschaeftList.addAll(telefonatTaskRepository.findAllByCardCategoryGeschaeft());
-        this.geschaeftList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryGeschaeft());
-        this.geschaeftList.addAll(sonstigesTaskRepository.findAllByCardCategoryGeschaeft());
-        this.geschaeftList.addAll(veranstaltungTaskRepository.findAllByCardCategoryGeschaeft());
-        this.geschaeftList.addAll(wanderkarteTaskRepository.findAllByCardCategoryGeschaeft());
-        return this.geschaeftList;
+    public List<GenericTaskEntity> findAllGeschäftszimmer() {
+        List<GenericTaskEntity> geschaeftList = new ArrayList<>();
+        geschaeftList.addAll(dringendTaskRepository.findAllByCardCategoryGeschaeft());
+        geschaeftList.addAll(telefonatTaskRepository.findAllByCardCategoryGeschaeft());
+        geschaeftList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryGeschaeft());
+        geschaeftList.addAll(sonstigesTaskRepository.findAllByCardCategoryGeschaeft());
+        geschaeftList.addAll(veranstaltungTaskRepository.findAllByCardCategoryGeschaeft());
+        geschaeftList.addAll(wanderkarteTaskRepository.findAllByCardCategoryGeschaeft());
+        return geschaeftList;
     }
 
     @GetMapping(path = "/langfristig")
-    public Iterable<GenericTaskEntity> findAllLangfristig() {
-        this.langfristigList.addAll(dringendTaskRepository.findAllByCardCategoryLang());
-        this.langfristigList.addAll(telefonatTaskRepository.findAllByCardCategoryLang());
-        this.langfristigList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryLang());
-        this.langfristigList.addAll(sonstigesTaskRepository.findAllByCardCategoryLang());
-        this.langfristigList.addAll(veranstaltungTaskRepository.findAllByCardCategoryLang());
-        this.langfristigList.addAll(wanderkarteTaskRepository.findAllByCardCategoryLang());
-        return this.langfristigList;
+    public List<GenericTaskEntity> findAllLangfristig() {
+        List<GenericTaskEntity> langfristigList = new ArrayList<>();
+        langfristigList.addAll(dringendTaskRepository.findAllByCardCategoryLang());
+        langfristigList.addAll(telefonatTaskRepository.findAllByCardCategoryLang());
+        langfristigList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryLang());
+        langfristigList.addAll(sonstigesTaskRepository.findAllByCardCategoryLang());
+        langfristigList.addAll(veranstaltungTaskRepository.findAllByCardCategoryLang());
+        langfristigList.addAll(wanderkarteTaskRepository.findAllByCardCategoryLang());
+        return langfristigList;
     }
 
     @GetMapping(path = "/done")
-    public Iterable<GenericTaskEntity> findAllDone() {
-        this.doneList.addAll(dringendTaskRepository.findAllByCardCategoryDone());
-        this.doneList.addAll(telefonatTaskRepository.findAllByCardCategoryDone());
-        this.doneList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryDone());
-        this.doneList.addAll(sonstigesTaskRepository.findAllByCardCategoryDone());
-        this.doneList.addAll(veranstaltungTaskRepository.findAllByCardCategoryDone());
-        this.doneList.addAll(wanderkarteTaskRepository.findAllByCardCategoryDone());
-        return this.doneList;
+    public List<GenericTaskEntity> findAllDone() {
+        List<GenericTaskEntity> doneList = new ArrayList<>();
+        doneList.addAll(dringendTaskRepository.findAllByCardCategoryDone());
+        doneList.addAll(telefonatTaskRepository.findAllByCardCategoryDone());
+        doneList.addAll(mitarbeiteranmeldenTaskRepository.findAllByCardCategoryDone());
+        doneList.addAll(sonstigesTaskRepository.findAllByCardCategoryDone());
+        doneList.addAll(veranstaltungTaskRepository.findAllByCardCategoryDone());
+        doneList.addAll(wanderkarteTaskRepository.findAllByCardCategoryDone());
+        return doneList;
     }
 
 }
